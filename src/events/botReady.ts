01976caf52ac2be7aacc0f07e8a95981aa417ddb
JustEvil <thatjustevil@gmail.com>
2024-09-12 15:57:39 -0600

    move line

--- src/events/botReady.ts
+++ src/events/botReady.ts
@@ -3,13 +3,14 @@ import container from "../container";
 import { createEvent } from "seyfert";
 import { Manager } from "../structures";
 
+const manager = container.get(Manager);
+
 export default createEvent({
     data: {
         name: "botReady",
     },
     run: async (user, client) => {
         const { logger } = client;
-        const manager = container.get(Manager);
 
         await manager.load();
         await manager.init({ id: client.botId, username: user.username });
